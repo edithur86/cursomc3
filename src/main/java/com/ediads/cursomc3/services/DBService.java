b5a9fb061c16f7e4da51bac85d62328ecc15d504
ediCarlos <ecarlos.leite@hotmail.com>
2021-09-09 00:04:33 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/ediads/cursomc3/services/DBService.java
+++ src/main/java/com/ediads/cursomc3/services/DBService.java
@@ -119,7 +119,7 @@ public void instantiateTestDatabase() throws ParseException {
 	estadoRepository.saveAll(Arrays.asList(est1, est2));
 	cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	
-	Cliente cli1 = new Cliente(null, "MariaSilva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+	Cliente cli1 = new Cliente(null, "MariaSilva", "ecarlos.leite@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 	
 	cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 	
